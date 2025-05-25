347aaf38cc5f835eb9505639c6a46ec88711541f
Samaniqbal02 <samanjutt59@gmail.com>
2025-05-25 12:13:22 +0500

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+ name: { type: String, required: true },
+ email: { type: String, required: true, unique: true },
+ age: { type: Number },
+}, { timestamps: true });
+const userModel= mongoose.model('User', userSchema);
+module.exports = userModel
